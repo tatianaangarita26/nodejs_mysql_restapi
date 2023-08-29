cfe1a30dee9144f5922c72c43fe15d549fafca47
tatianaangarita26 <tatianaangarita150@gmail.com>
2023-08-29 07:36:23 -0500

    Carga de archivos a repo

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -2,30 +2,33 @@ import { pool } from '../db.js'
 
 export const getEmployees = async (req,res) => {
     try{
-    const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?",[
+    const [rows] = await pool.query('SELECT * FROM employee')
+    res.json(rows)
+    }catch (error){
+        return res.status(500).json({
+            message:'Algo va mal'
+        })
+    }
+}
+
+
+export const getEmployee = async (req,res) => {
+    try{
+    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [
         req.params.id,
     ]);
 
-    if (rows.length <= 0)
+    if(rows.length <= 0) 
     return res.status(404).json({
         message: "Empleado no encontrado",
     });
     res.json(rows[0]);
-    } catch (error) {
-        return res.status(500).json({
-            message:'Algo va mal',
-        });
-    }
-};
-
-export const getEmployee = async (req,res) => {
-    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
-
-    if(rows.length <= 0) return res.status(404).json({
-        message: 'Empleado no encontrado'
-    })
-    res.json(rows[0])
+} catch (error){
+    return res.status(500).json({
+        message: "Algo va mal",
+    });
 }
+};
 
 export const createEmployees = async (req,res) => {
     try{
